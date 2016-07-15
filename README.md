9ab6c1d119e5bc5bd9685529a71a21d258e877b8
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-07-15 11:51:35 +0200

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Development Helpers for Strider
+
+## `clone-strider-org`
+Clones all repositories in the **strider-cd** GitHub organization into the current working directory.
+
+- `--https` Use HTTPS for the cloning process. The default is to the the SSH URL.
+- `--jobs=n` Set the amount of concurrent cloning processes to run. Default is **6**.
---
+++ bin/clone-strider-org.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const Promise = require("bluebird");
+
+const argv = require("minimist")(process.argv.slice(2));
+const execa = require("execa");
+const got = require("got");
+
+const STRIDER_ORG_REPOS = "https://api.github.com/orgs/strider-cd/repos";
+const CONCURRENT_JOBS = argv.jobs || 6;
+
+console.log("Cloning all repositories in the strider-cd organization…");
+got(STRIDER_ORG_REPOS, {json: true})
+    .then(response => {
+        return Promise.map(response.body, cloneRepo, {
+            concurrency: CONCURRENT_JOBS
+        });
+    })
+    .then(count => console.log(`Operation completed. Cloned ${count.length} repositories.`))
+    .catch(error => {
+        console.error(error);
+    });
+
+function cloneRepo(repo) {
+    console.log(`  Cloning '${repo.full_name}'…`);
+
+    const urlToUse = argv.https ? repo.clone_url : repo.ssh_url;
+
+    return execa("git", ["clone", urlToUse]);
+}
