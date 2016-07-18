af89abea618d278969f84d0eab5938e687e0fc94
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-07-18 20:07:14 +0200

    Use access token

--- bin/needs-release.js
+++ bin/needs-release.js
@@ -16,13 +16,18 @@ if (!GITHUB_ACCESS_TOKEN) {
   process.exit(1);
 }
 
-const STRIDER_ORG_REPOS = `https://api.github.com/orgs/strider-cd/repos?per_page=200&access_token=${GITHUB_ACCESS_TOKEN}`;
-
+const STRIDER_ORG_REPOS = 'https://api.github.com/orgs/strider-cd/repos';
 
 console.log('Checking for repos with commits after latest tag…');
-got(STRIDER_ORG_REPOS, {json: true})
+got(STRIDER_ORG_REPOS, {
+  json: true,
+  query: {
+    per_page: 200,
+    access_token: GITHUB_ACCESS_TOKEN
+  }
+})
   .then(response => {
-    return Promise.each(response.body, testRepo);
+    return Promise.map(response.body, testRepo, {concurrency: 1});
   })
   .then(count => console.log(`Operation completed. ${count.filter(Boolean).length} repositories need release. Total projects: ${count.length}`))
   .catch(error => {
@@ -31,7 +36,12 @@ got(STRIDER_ORG_REPOS, {json: true})
 
 function testRepo(repo) {
   console.log(`  ${repo.name}:`);
-  return Promise.resolve(got(`https://api.github.com/repos/strider-cd/${repo.name}/tags?access_token=${GITHUB_ACCESS_TOKEN}`, {json: true}))
+  return Promise.resolve(got(`https://api.github.com/repos/strider-cd/${repo.name}/tags`, {
+    json: true,
+    query: {
+      access_token: GITHUB_ACCESS_TOKEN
+    }
+  }))
     .then(response => {
       const tags = response.body;
       if (!tags || !tags.length) {
@@ -39,11 +49,16 @@ function testRepo(repo) {
         return true;
       }
 
-      return got(`https://api.github.com/repos/strider-cd/${repo.name}/commits`, {json: true})
+      return got(`https://api.github.com/repos/strider-cd/${repo.name}/commits`, {
+        json: true,
+        query: {
+          access_token: GITHUB_ACCESS_TOKEN
+        }
+      })
         .then(response => {
           const commits = response.body;
           if (commits[0].sha !== tags[0].commit.sha) {
-            console.log(`    RELEASE: SHA mismatch: ${commits[0].sha} - ${tags[0].commit.sha}`);
+            console.log(`    RELEASE: SHA mismatch:  tag:${tags[0].commit.sha} → latest:${commits[0].sha}`);
             return true;
           }
 
@@ -53,6 +68,5 @@ function testRepo(repo) {
         });
 
     })
-    .catch({statusCode: 403}, () => console.log(`    UNKNOWN: 403 error`))
-    .return(false);
+    .catch({statusCode: 403}, () => console.log(`    UNKNOWN: 403 error`));
 }
